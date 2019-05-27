1acb63839962b2b2347c3dfb4217fd798a43f6e8
roshankc123 <47151288+roshankc123@users.noreply.github.com>
2019-05-27 13:06:40 +0545

    Add files via upload

---
+++ bct075/home.php
@@ -0,0 +1,35 @@
+<?php    //start echo
+echo ' 
+<html>
+<head></head>   
+<title>PASBCT075</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style/style_homepage.css">
+<script type="text/javascript" src="script/homepage.js"></script>
+</head>
+<body>
+   <div id="form_1" onmouseover="text_move()">
+      <u id="u_all">BCT-075</u>
+      <span class="profile_display"><h5>'."roshankc".'</h5></span>
+      <span class="hello"><h3 class="activities"><i class="fa fa-bell"></i>Notifications</h3><h3 class="messages"><i class="fa fa-envelope"></i>Messages</h3></span>
+      <h4 class="profile"><i class="glyphicon glyphicon-user"></i>  Profile</h4>
+      
+   </div>
+  <span class="main">
+        <div id="side" >I am in side
+           <br> hello don
+           <i >am also don
+
+           </i>
+        </div>
+        <div id="middle">
+            <strong>I am in middle!</strong>
+        </div>
+        <div id="sidebar">I am in side bar</div>
+  </span>
+   
+   <div id="footer">copyright @wrc_bct_075</div>
+</body>
+</html> '; //end echo
+?>
