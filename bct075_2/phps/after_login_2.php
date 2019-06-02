d6a3c9a5e4503b7b940450cb58883a6dd7558c7f
roshankc123 <47151288+roshankc123@users.noreply.github.com>
2019-06-02 17:12:33 +0545

    Add files via upload

---
+++ bct075_2/phps/after_login_2.php
@@ -0,0 +1,14 @@
+<?php
+  //session manager///
+  session_start();
+  $id=$_SESSION['session'];
+  ////////////////////
+  //import from sql///
+  $db_0=mysqli_connect("localhost","root","hellow0rl_d","bct075");
+  if(!$db_0)echo mysqli_connect_error();
+  $db_1=mysqli_query($db_0,"select * from second where session='".$id."';");
+  $data_sql=mysqli_fetch_assoc($db_1);
+  $_SESSION['u_name']=$data_sql['u_name'];
+  header("location: ../home.php");
+  //echo Time();
+?>
